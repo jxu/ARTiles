6ca88803f80b9193e4c9845cede53ad273c673ba
wxv <wxv@users.noreply.github.com>
2017-10-18 23:12:44 -0400

    Start basic poster support

---
+++ Assets/Scripts/Poster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poster : MonoBehaviour 
+{
+	public float realSizeWidth;
+	public float realSizeHeight;
+
+
+	void Start () 
+	{
+		// Scale sprite from arbitrary pixel size to Unity units
+		SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+		Sprite sp = renderer.sprite;
+		float pixelsPerUnit = sp.pixelsPerUnit;
+
+		float spUnitsWidth = sp.rect.width / pixelsPerUnit;
+		float spUnitsHeight = sp.rect.height / pixelsPerUnit;
+
+		print ("Sprite width height (units): " + spUnitsWidth + " " + spUnitsHeight);
+
+		transform.localScale = new Vector3 (realSizeWidth / spUnitsWidth, 
+			                          		realSizeHeight / spUnitsHeight, 
+											transform.localScale.z);
+
+	}
+}
