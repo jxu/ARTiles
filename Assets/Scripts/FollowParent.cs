40a86f2b1100449c78c6e592d195bc8ebd06d1d3
wxv <wxv@users.noreply.github.com>
2017-10-20 18:03:45 -0400

    Remove unneeded follow parent script from dishware, rename Poster

--- Assets/Scripts/FollowParent.cs
+++ Assets/Scripts/FollowParent.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Should not be needed if model is child of image target
+
 public class FollowParent : MonoBehaviour {
-
-
 	void Update () 
 	{
 		gameObject.transform.localPosition = new Vector3 (0f, 0f, 0f);
