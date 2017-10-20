db1c4357339b9735800ee4152fe3b10bfd50eeb3
wxv <wxv@users.noreply.github.com>
2017-10-20 17:56:15 -0400

    Enable dishware to track image targets independently

---
+++ Assets/Scripts/FollowParent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowParent : MonoBehaviour {
+
+
+	void Update () 
+	{
+		gameObject.transform.localPosition = new Vector3 (0f, 0f, 0f);
+	}
+}
