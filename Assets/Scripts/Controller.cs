eeb704ee74ace8b543a409efa5862fecefeddb49
wxv <wxv@users.noreply.github.com>
2017-11-19 18:50:20 -0500

    Refactor controller to work for posters and other objects

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿// TODO: Better selection interface (bounding box?)
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -6,13 +8,13 @@ using System.Linq;
 public class Controller : MonoBehaviour {
 	
 	public Material spritesDefault;
-	public Material grayscale;
+	//public Material highlightMaterial;
 
-	private int selectedTarget = 0;
-	private string indexText;
-	private Object[] allSprites;
-	private GameObject[] allImageTargets;
-	private int imageTargetsCount;
+	int selectedTarget = 0;
+	string indexText;
+	List<Object> allSprites;
+	List<GameObject> allImageTargets;
+	int imageTargetsCount;
 
 	const float realHeightDelta = 0.1f;
 	const float realWidthDelta = 0.1f;
@@ -33,92 +35,110 @@ public class Controller : MonoBehaviour {
 	// Warning: Image targets added out of order!
 	void initAllImageTargets()
 	{
-		allImageTargets = GameObject.FindGameObjectsWithTag ("ImageTarget");
-		imageTargetsCount = allImageTargets.Length;
+		allImageTargets = GameObject.FindGameObjectsWithTag("ImageTarget").ToList();
+		imageTargetsCount = allImageTargets.Count;
 
 		foreach (GameObject it in allImageTargets) 
-			Debug.Log("Found " + it);
+			Debug.Log("Found ImageTarget " + it);
 	}
 
-	// All sprites in Resources/Posters will be loaded 
-	// in alphabetical order, for consistency
 	void initAllSprites()
 	{
-		// Load sprite resources
-		allSprites = Resources.LoadAll("Posters", typeof(Sprite));
+		// All sprites in Resources/Posters will be loaded in alphabetical order, for consistency
+		allSprites = Resources.LoadAll("Posters", typeof(Sprite)).ToList();
 		foreach(Object s in allSprites)
 			Debug.Log("Found sprite " + s);
 
-		allSprites = allSprites.OrderBy(s => s.ToString()).ToArray();
+		allSprites = allSprites.OrderBy(s => s.ToString()).ToList();
 	}
 
 	void handleInput()
 	{
-		Transform posterTransform = allImageTargets[selectedTarget].transform.GetChild(0);
-		Poster poster = posterTransform.GetComponent<Poster>();
+		GameObject obj = getTargetChild(selectedTarget);
 
-
-		// Cycle selection
+		// Cycle selection with left/right
 		if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
 		{
 			if (Input.GetKeyDown(KeyCode.LeftArrow))
 				selectedTarget = (selectedTarget - 1 + imageTargetsCount) % imageTargetsCount;
 
-			if (Input.GetKeyDown(KeyCode.RightArrow))
+			else if (Input.GetKeyDown(KeyCode.RightArrow))
 				selectedTarget = (selectedTarget + 1) % imageTargetsCount;
+
+			obj = getTargetChild(selectedTarget);
 
 			// TODO: inefficient, fix later
 			indexText = "Target Index: " + selectedTarget.ToString();
 
 			// TODO: Have initial selection highlighted
+			// Update highlight current selection in grayscale
+			/*
 			for(int i = 0; i < imageTargetsCount; i++)
 			{
-				Transform thisPosterTransform = allImageTargets[i].transform.GetChild(0);
-				Renderer renderer = thisPosterTransform.GetComponent<Renderer>();
-				renderer.material = (i == selectedTarget) ? grayscale : spritesDefault;
-			}
+				Renderer renderer = obj.GetComponent<Renderer>();
+				renderer.material = (i == selectedTarget) ? highlightMaterial : spritesDefault;
+			}*/
+
 		}
 
 		// Cycle poster choice
 		else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
 		{
-			int spriteCount = allSprites.Length;
+			if (obj.CompareTag("Poster"))
+			{
+				Transform posterTransform = obj.transform;
+				Poster poster = posterTransform.GetComponent<Poster>();
+				
+				int spriteCount = allSprites.Count;
 
-			if (Input.GetKeyDown(KeyCode.UpArrow))
-				poster.spriteIndex = (poster.spriteIndex + 1) % spriteCount;
+				if (Input.GetKeyDown(KeyCode.UpArrow))
+					poster.spriteIndex = (poster.spriteIndex + 1) % spriteCount;
 
-			if (Input.GetKeyDown(KeyCode.DownArrow))
-				poster.spriteIndex = (poster.spriteIndex - 1 + spriteCount) % spriteCount;
+				if (Input.GetKeyDown(KeyCode.DownArrow))
+					poster.spriteIndex = (poster.spriteIndex - 1 + spriteCount) % spriteCount;
 
-			// Replace sprite with sprite from index
-			posterTransform.GetComponent<SpriteRenderer>().sprite = 
-				(Sprite) allSprites[poster.spriteIndex];
-			
-			poster.ScaleReal();
+				// Replace sprite with sprite from index
+				posterTransform.GetComponent<SpriteRenderer>().sprite = 
+					(Sprite) allSprites[poster.spriteIndex];
+
+				poster.ScaleReal();
+			}
 		}
 
-		// Change size of poster
+		// Change size of poster with WASD
 		else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || 
 			Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
 		{
-			if (Input.GetKeyDown(KeyCode.W))
-				poster.realHeight += realHeightDelta;
+			if (obj.CompareTag("Poster"))
+			{
+				Poster poster = obj.GetComponent<Poster>();
 
-			if (Input.GetKeyDown(KeyCode.S) && 
-				poster.realHeight - realHeightDelta > minPosterLength)
-				poster.realHeight -= realHeightDelta;
+				if (Input.GetKeyDown(KeyCode.W))
+					poster.realHeight += realHeightDelta;
 
-			if (Input.GetKeyDown(KeyCode.D))
-				poster.realWidth += realWidthDelta;
+				if (Input.GetKeyDown(KeyCode.S) && 
+					poster.realHeight - realHeightDelta > minPosterLength)
+					poster.realHeight -= realHeightDelta;
 
-			if (Input.GetKeyDown(KeyCode.A) && 
-				poster.realWidth - realWidthDelta > minPosterLength)
-				poster.realWidth -= realWidthDelta;
+				if (Input.GetKeyDown(KeyCode.D))
+					poster.realWidth += realWidthDelta;
 
-			poster.ScaleReal();
+				if (Input.GetKeyDown(KeyCode.A) && 
+					poster.realWidth - realWidthDelta > minPosterLength)
+					poster.realWidth -= realWidthDelta;
+
+				poster.ScaleReal();
+			}
 		}
 
 	}
+
+
+	GameObject getTargetChild(int ind)
+	{
+		return allImageTargets[ind].transform.GetChild(0).gameObject;
+	}
+		
 
 	// Immediate Mode GUI, for debugging (see GUI Scripting Guide)
 	void OnGUI()
